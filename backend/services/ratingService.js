5b589aa92d491584cc172df9e1a495ceda30fbbc
thanhtung2415 <thanhtungkid2415@gmail.com>
2026-03-31 15:00:33 +0700

    fix_loi

--- backend/services/ratingService.js
+++ backend/services/ratingService.js
@@ -3,7 +3,7 @@ const db = require('../db');
 class RatingService {
   async addRating(data) {
     return db('ratings').insert({ 
-      user_id: 1, 
+      user_id: data.userId, 
       game_slug: data.gameSlug, 
       rating: data.rating, 
       comment: data.comment 
